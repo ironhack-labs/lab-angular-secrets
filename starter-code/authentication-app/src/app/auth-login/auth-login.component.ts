9f84452517f461c17761bd49fc51ff709cee462f
Fernando Palma <ferpalma21@github.com>
2017-07-28 13:06:05 +0200

    iteration 5 not working at all

--- starter-code/authentication-app/src/app/auth-login/auth-login.component.ts
+++ starter-code/authentication-app/src/app/auth-login/auth-login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { SessionService } from '../../services/session.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-auth-login',
@@ -6,8 +8,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./auth-login.component.css']
 })
 export class AuthLoginComponent implements OnInit {
-  constructor() { }
+  user: any;
+  formInfo = {
+    username: '',
+    password: ''
+  };
+  error: string;
+  constructor(private session: SessionService, private router : Router) { }
 
-  ngOnInit() {
+ ngOnInit() {
+  }
+
+ login() {
+    this.session.login(this.formInfo)
+      .subscribe(
+        (user) => this.successCb(user),
+        (err) => this.errorCb(err)
+      );
+      this.router.navigate(['private'])
+  }
+
+ errorCb(err) {
+    this.error = err;
+    this.user = null;
+  }
+
+ successCb(user) {
+    this.user = user;
+    this.error = null;
   }
 }
