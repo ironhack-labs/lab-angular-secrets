12cdd52ea682d48c5080d6d943250ec7845e9ac9
ferrodri <f.rodriguez.hervias@gmail.com>
2017-07-28 12:55:00 +0200

    on process

--- starter-code/authentication-app/src/app/app.component.ts
+++ starter-code/authentication-app/src/app/app.component.ts
@@ -1,14 +1,16 @@
-import { Component, OnInit } from '@angular/core';
-import { Observable } from "rxjs/Rx";
+import { Component } from '@angular/core';
+import { SessionService } from './session.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-  constructor() { }
+export class AppComponent {
+  title = 'Secrets';
+  constructor(private session:SessionService){ }
 
-  ngOnInit() {
+  logout(){
+    this.session.logout().subscribe();
   }
 }
