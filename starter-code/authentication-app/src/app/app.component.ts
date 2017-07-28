89da2367c5031a99c15368c07a7fddec7a5e58d5
Ana Milojevic <anamilojevicd@gmail.com>
2017-07-28 13:10:27 +0200

    iteration 4 done

--- starter-code/authentication-app/src/app/app.component.ts
+++ starter-code/authentication-app/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from "rxjs/Rx";
+import { SessionService } from "./session.service";
 
 @Component({
   selector: 'app-root',
@@ -7,8 +8,10 @@ import { Observable } from "rxjs/Rx";
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  constructor() { }
+  constructor(private session: SessionService) { }
 
   ngOnInit() {
+    this.session.login('Fran B', '5678')
+      .subscribe(e => console.log(e))
   }
 }
