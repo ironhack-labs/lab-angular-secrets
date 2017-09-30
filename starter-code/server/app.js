c207e787b96833b3fa93dcb9e37ee91c9fbed1c6
raquel500c <raquel500c@gmail.com>
2017-09-30 02:48:05 +0200

    Iteration 3

--- starter-code/server/app.js
+++ starter-code/server/app.js
@@ -1,24 +1,20 @@
 const express = require('express');
 const path = require('path');
 const favicon = require('serve-favicon');
-const session    = require('express-session');
+const session = require('express-session');
 const MongoStore = require('connect-mongo')(session);
 const logger = require('morgan');
-const passport   = require('passport');
+const passport = require('passport');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
-const debug = require('debug')("angularauth:"+path.basename(__filename).split('.')[0]);
+const debug = require('debug')("angularauth:" + path.basename(__filename).split('.')[0]);
 const authController = require('./routes/authController');
 const mongoose = require('mongoose');
 const cors = require('cors');
 
-const app            = express();
+const app = express();
 
 require('./config/database');
-
-
-// Passport configuration
-require("./config/passport")(passport);
 
 var whitelist = [
     'http://localhost:4200',
@@ -38,10 +34,6 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 
-app.use(passport.initialize());
-app.use(passport.session());
-
-// uncomment after placing your favicon in /public
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -52,14 +44,18 @@ app.use(session({
   secret: "lab-angular-authentication",
   resave: true,
   saveUninitialized: true,
-  cookie: { httpOnly: true, maxAge: 2419200000 },
+  cookie: { httpOnly: true, maxAge: 60*60*24*365 },
   store:new  MongoStore ({mongooseConnection: mongoose.connection})
 }));
 
+require("./config/passport")(passport);
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use('/api', authController);
-app.all('/*', (req, res) => {
-  res.sendFile(__dirname + '/public/index.html');
-});
+// app.all('/*', (req, res) => {
+//   res.sendFile(__dirname + '/public/index.html');
+// });
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -72,12 +68,11 @@ app.use(function(req, res, next) {
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  // res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
   console.error(err);
   // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
-
 
 module.exports = app;
--- starter-code/server/routes/authController.js
+++ starter-code/server/routes/authController.js
@@ -8,66 +8,35 @@ const bcryptSalt     = 19;
 const authController = express.Router();
 
 authController.post("/signup", (req, res, next) => {
-  let username = req.body.username;
-  let password = req.body.password;
-  let name     = req.body.name;
-  let secret   = req.body.secret;
+  const username = req.body.username;
+  const password = req.body.password;
+  const name     = req.body.name;
+  const secret   = req.body.secret;
 
   if (!username || !password || !name || !secret) {
     res.status(400).json({ message: "Provide all the fields to sign up" });
   }
 
-  User.findOne({username}, "username")
-    .then(user => {
-      if (user !== null) {
-        return res.status(400).json({ message: "The username already exists" });
-      }
-      let salt     = bcrypt.genSaltSync(bcryptSalt);
-      let hashPass = bcrypt.hashSync(password, salt);
+  User.findOne({ username },'_id').exec().then(user =>{
+     if(user)
+       return res.status(400).json({ message: 'The username already exists' });
 
-      let newUser  = User({
+      const salt     = bcrypt.genSaltSync(bcryptSalt);
+      const hashPass = bcrypt.hashSync(password, salt);
+
+      const theUser = new User({
         username,
         password: hashPass,
         name,
         secret
       });
-
-      newUser.save()
+      return theUser.save()
         .then(user => res.status(200).json(req.user))
         .catch(e => res.status(500).json({ message: "Something went wrong" }))
     }).catch(e => res.status(500).json({ message: e }))
-
-  // User.findOne({ username }, "username", (err, user) => {
-  //   if (user !== null) {
-  //     res.status(400).json({ message: "The username already exists" });
-  //     return;
-  //   }
-  //
-  //   let salt     = bcrypt.genSaltSync(bcryptSalt);
-  //   let hashPass = bcrypt.hashSync(password, salt);
-  //
-  //   let newUser  = User({
-  //     username,
-  //     password: hashPass,
-  //     name,
-  //     secret
-  //   });
-  //
-  //   console.log(newUser);
-  //
-  //   newUser.save((err) => {
-  //     if (err) { res.status(400).json({ message: "Something went wrong" }); }
-  //     else {
-  //       req.login(newUser, (err) => {
-  //         if (err) { return res.status(500).json({ message: "Something went wrong" }); }
-  //         res.status(200).json(req.user);
-  //       });
-  //     }
-  //   });
 });
 
 authController.post("/login", (req, res, next) => {
-  console.log("entrooo")
   passport.authenticate('local', (err, user, failureDetails) => {
     if (err)
       return res.status(500).json({ message: 'Something went wrong' });
@@ -76,7 +45,6 @@ authController.post("/login", (req, res, next) => {
       return res.status(401).json(failureDetails);
 
     req.login(user, (err) => {
-      console.log("llego a login")
       if (err)
         return res.status(500).json({ message: 'Something went wrong' });
 
@@ -92,8 +60,9 @@ authController.get("/logout", (req, res) => {
 });
 
 authController.get("/loggedin", (req, res) => {
-  if (req.isAuthenticated()) { return res.status(200).json(req.user); }
-  return res.status(403).json({ message: "Unauthorized" });
+  if (req.isAuthenticated())
+   return res.status(200).json(req.user);
+  res.status(403).json({ message: "Unauthorized" });
 });
 
 authController.get("/private", (req, res) => {
