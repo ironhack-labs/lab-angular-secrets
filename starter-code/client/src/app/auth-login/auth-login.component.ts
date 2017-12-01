3073055e77cb32bffdd6dba726142ba6b0cc0e26
Yurema Rebollo <yuremarc@gmail.com>
2017-12-01 19:35:30 +0100

    It5, no nos pinta

--- starter-code/client/src/app/auth-login/auth-login.component.ts
+++ starter-code/client/src/app/auth-login/auth-login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {SessionService} from '../session.service';
+import {User } from '../shared/models/user.model';
 
 @Component({
   selector: 'app-auth-login',
@@ -6,8 +8,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./auth-login.component.css']
 })
 export class AuthLoginComponent implements OnInit {
-  constructor() { }
+  sessions:any;
+  user: User;
+  constructor(public sessionService:SessionService) { }
 
   ngOnInit() {
-  }
+    // this.sessionService.login(this.user).subscribe( user =>{
+    //   console.log(user);
+    //   this.sessions = user;
+    // }
+  // );
+  //
+// ⁄
+}
 }
