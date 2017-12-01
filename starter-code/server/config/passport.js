465e2c05f32b25e19872f8c3790d4bb4ce51740e
Alejandro Dietta <alexdietta@hotmail.com>
2017-12-01 17:35:28 +0100

    OMG 2 done

--- starter-code/server/config/passport.js
+++ starter-code/server/config/passport.js
@@ -8,7 +8,7 @@ module.exports = (passport) => {
       if (err) { return next(err); }
       if (!user) { next(null, false, { message: "Incorrect username" }); }
       if (!bcrypt.compareSync(password, user.password)) { next(null, false, { message: "Incorrect password" });
-    }
+    } 
 
       return next(null, user);
     });
