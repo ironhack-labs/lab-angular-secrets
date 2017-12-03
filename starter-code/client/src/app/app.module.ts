21a5d41e44a24ee6f5c166d85f16a3c46d45405c
Anyelina Moreno Robledo <anyelinamoreno@gmail.com>
2017-12-04 00:26:22 +0100

    Iteration 9: Private Component

--- starter-code/client/src/app/app.module.ts
+++ starter-code/client/src/app/app.module.ts
@@ -8,6 +8,7 @@ import { AppComponent } from './app.component';
 import { AuthLoginComponent } from './auth-login/auth-login.component';
 import { AuthSignupComponent } from './auth-signup/auth-signup.component';
 import { MyPrivatePageComponent } from './my-private-page/my-private-page.component';
+import { IsAuthenticatedGuard } from './guards/is-authenticated.guard';
 
 import { RouterModule } from '@angular/router';
 
