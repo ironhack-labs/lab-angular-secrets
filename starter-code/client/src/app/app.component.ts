7546a353013dbe8ad9540b5b419c0e09de1995f2
Anna Hogberg <am.hogberg@gmail.com>
2018-07-27 16:51:47 +0200

    done

--- starter-code/client/src/app/app.component.ts
+++ starter-code/client/src/app/app.component.ts
@@ -1,14 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { Observable } from "rxjs/Rx";
+import { SessionService } from './session.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-  constructor() { }
-
-  ngOnInit() {
+export class AppComponent{
+  constructor(public sessionService: SessionService) { }
+  logout(){
+    this.sessionService.logout().subscribe();
   }
+
 }
--- starter-code/server/app.js
+++ starter-code/server/app.js
@@ -8,8 +8,23 @@ const cors           = require("cors");
 const authController = require("./routes/authController");
 const session        = require("express-session");
 const passport       = require("passport");
+const MongoStore = require('connect-mongo')(session);
+
 
 const app            = express();
+
+// Middleware Setup
+var whitelist = [
+  'http://localhost:4200',
+];
+var corsOptions = {
+  origin: function(origin, callback){
+      var originIsWhitelisted = whitelist.indexOf(origin) !== -1;
+      callback(null, originIsWhitelisted);
+  },
+  credentials: true
+};
+app.use(cors(corsOptions)); 
 
 // Passport configuration
 require("./config/passport")(passport);
@@ -23,12 +38,12 @@ app.use(session({
   secret: "lab-angular-authentication",
   resave: true,
   saveUninitialized: true,
-  cookie: { httpOnly: true, maxAge: 2419200000 }
+  cookie: { httpOnly: true, maxAge: 2419200000 },
+  store: new MongoStore({ mongooseConnection: mongoose.connection })
 }));
 
 app.use(passport.initialize());
 app.use(passport.session());
-app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
 
 // uncomment after placing your favicon in /public
@@ -37,7 +52,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use('/api', authController);
+app.use('/api/auth', authController);
 app.all('/*', (req, res) => {
   res.sendFile(__dirname + '/public/index.html');
 });
